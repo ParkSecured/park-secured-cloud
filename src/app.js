162e751ab28aef683307f53600d310492843ed7f
rzvmns <razvan.manescu@student.upt.ro>
2026-06-08 12:33:33 +0300

    Error check

--- src/app.js
+++ src/app.js
@@ -48,4 +48,11 @@ app.use('/api', gateRoutes);
 app.use('/api', reportRoutes);
 app.use('/api', testRoutes);
 
+app.use((err, req, res, next) => {
+    if (err.type === 'entity.parse.failed') {
+        return res.status(400).json({ success: false, message: 'JSON invalid în request.' });
+    }
+    next(err);
+});
+
 module.exports = app;
